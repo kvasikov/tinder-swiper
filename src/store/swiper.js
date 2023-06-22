4f119481c14fcea4bbabd60650654bfb832b7d19
b0006 <qsefthuken@gmail.com>
2023-06-22 19:38:46 +0700

    refactor: minor

--- src/store/swiper.js
+++ src/store/swiper.js
@@ -1,10 +1,10 @@
-import { action, configure, makeAutoObservable, observable } from 'mobx';
+import { action, configure, makeAutoObservable, observable, runInAction } from 'mobx';
 
 configure({
   enforceActions: 'always',
   computedRequiresReaction: true,
   reactionRequiresObservable: true,
-  observableRequiresReaction: true,
+  observableRequiresReaction: false,
   disableErrorBoundaries: true,
 });
 
@@ -13,20 +13,28 @@ export class SwiperStore {
   swiperInstance = null;
 
   constructor() {
-    makeAutoObservable(this, {
-      profileList: observable,
-      swiperInstance: observable,
-      setSwiperInstance: action,
-      setProfileList: action,
-    });
+    makeAutoObservable(
+      this,
+      {
+        profileList: observable,
+        swiperInstance: observable,
+        setSwiperInstance: action,
+        setProfileList: action,
+      },
+      { autoBind: true },
+    );
   }
 
   setSwiperInstance = (swiperInstance) => {
-    this.swiperInstance = swiperInstance;
+    runInAction(() => {
+      this.swiperInstance = swiperInstance;
+    });
   };
 
   setProfileList = (profileList = []) => {
-    this.profileList = profileList;
+    runInAction(() => {
+      this.profileList = profileList;
+    });
   };
 }
 
