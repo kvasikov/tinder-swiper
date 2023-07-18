edc6f6513ec7b57c0990fc963ab08a1229ccb130
b0006 <qsefthuken@gmail.com>
2023-07-19 00:23:12 +0700

    minor

--- src/components/SwiperLayout/ProfileSwiper/TopBlock/TopBlock.jsx
+++ src/components/SwiperLayout/ProfileSwiper/TopBlock/TopBlock.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { observer } from 'mobx-react';
+import { swiperStore } from '../../store';
+import { useIsDesktop } from '../../../../hooks';
 import { TabsBlock } from './TabsBlock';
 import { HeaderBlock } from './HeaderBlock';
 import styles from './TopBlock.module.scss';
 
-export const TopBlock = () => {
+export const TopBlock = observer(() => {
+  const isDesktop = useIsDesktop();
+
+  const mobileStyles = !isDesktop
+    ? {
+        height: `${swiperStore.offsetTop}px`,
+        zIndex: !swiperStore.isHideMoreProfileInfo && 1,
+      }
+    : {};
+
   return (
-    <div className={styles.box}>
+    <div className={styles.box} style={mobileStyles}>
       <HeaderBlock />
       <TabsBlock />
     </div>
   );
-};
+});
