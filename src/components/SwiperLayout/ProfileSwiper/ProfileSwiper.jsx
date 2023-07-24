d6a731c2003fa16a65ed14fc49843a7ba442918f
b0006 <qsefthuken@gmail.com>
2023-07-25 00:24:31 +0700

    ref

--- src/components/SwiperLayout/ProfileSwiper/ProfileSwiper.jsx
+++ src/components/SwiperLayout/ProfileSwiper/ProfileSwiper.jsx
@@ -1,11 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
 import { toJS } from 'mobx';
 import cn from 'classnames';
 import { observer } from 'mobx-react';
 import { Spin, Space } from 'antd';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Virtual, Controller } from 'swiper';
-import 'swiper/css';
+import { Virtual, Controller } from 'swiper/modules';
+import { Swiper } from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/virtual';
 import { DATA_ATTR_PROFILE_ID } from '../../../constants/attributes';
 import { useDelayEffect, useIsDesktop } from '../../../hooks';
 import { swiperStore, getProfileIdByDataAttr } from '../store';
@@ -17,9 +18,12 @@ import { useGetOffsetTop } from './useGetOffsetTop.hook';
 import { useGetProfileList } from './useGetProfileList.hook';
 import { useWheelSwipe } from './useWheelSwipe.hook';
 
-SwiperCore.use([Virtual, Controller]);
+// SwiperCore.use([Virtual, Controller]);
 
 export const ProfileSwiper = observer(({ swiperState, setSwiperState }) => {
+  const swiperElRef = useRef(null);
+  const wasInitRef = useRef(false);
+
   const isDesktop = useIsDesktop();
   const [isActive] = useDelayEffect({ dependencyFlag: !swiperStore.isHideMoreProfileInfo });
 
@@ -30,37 +34,44 @@ export const ProfileSwiper = observer(({ swiperState, setSwiperState }) => {
     currentProfileDataId: swiperStore.currentProfileDataId,
   });
 
-  const onSliderChange = (swiper) => {
-    const prevProfileDataId = toJS(swiperStore.currentProfileData.id);
+  const onSliderChange = useCallback(
+    (swiper) => {
+      console.log('onSliderChange');
 
-    const isPrev =
-      prevIndex.current - swiper.activeIndex === 1 || prevIndex.current === swiper.activeIndex;
+      const prevProfileDataId = toJS(swiperStore.currentProfileData.id);
 
-    const actualIndex = swiper.visibleSlides.length > 1 ? swiper.visibleSlides.length - 1 : 0;
-    const currentIndex = isPrev ? 0 : actualIndex;
+      const isPrev =
+        prevIndex.current - swiper.activeIndex === 1 || prevIndex.current === swiper.activeIndex;
 
-    const profileId = getProfileIdByDataAttr(
-      swiper?.visibleSlides?.[currentIndex],
-      swiperStore.profileList?.[0].id,
-    );
+      const actualIndex = swiper.visibleSlides.length > 1 ? swiper.visibleSlides.length - 1 : 0;
+      const currentIndex = isPrev ? 0 : actualIndex;
 
-    swiperStore.setCurrentProfileId(profileId);
-    prevIndex.current = swiper.activeIndex;
+      const profileId = getProfileIdByDataAttr(
+        swiper?.visibleSlides?.[currentIndex],
+        swiperStore.profileList?.[0].id,
+      );
 
-    const profileData = swiperStore.profileList.find(
-      (profile) => profile.id === swiperStore.currentProfileDataId,
-    );
+      console.log({ visibleSlides: swiper.visibleSlides })
 
-    if (profileData?.isTweet === null && !isPrev) {
-      setTimeout(() => {
-        swiperStore.updateProfileData(prevProfileDataId, { isTweet: false });
-      }, 500);
-    }
+      swiperStore.setCurrentProfileId(profileId);
+      prevIndex.current = swiper.activeIndex;
 
-    if (swiper.virtual.slides.length - swiper.realIndex <= 1) {
-      fetchDataList();
-    }
-  };
+      const profileData = swiperStore.profileList.find(
+        (profile) => profile.id === swiperStore.currentProfileDataId,
+      );
+
+      // if (profileData?.isTweet === null && !isPrev) {
+      //   setTimeout(() => {
+      //     swiperStore.updateProfileData(prevProfileDataId, { isTweet: false });
+      //   }, 500);
+      // }
+
+      // if (swiper.virtual.slides.length - swiper.realIndex <= 1) {
+      //   fetchDataList();
+      // }
+    },
+    [fetchDataList],
+  );
 
   useWheelSwipe({
     swiperState,
@@ -68,6 +79,36 @@ export const ProfileSwiper = observer(({ swiperState, setSwiperState }) => {
     activeTabValue: swiperStore.activeTabValue,
     isHideMoreProfileInfo: swiperStore.isHideMoreProfileInfo,
   });
+
+  useEffect(() => {
+    if (!swiperElRef.current || wasInitRef.current) {
+      return;
+    }
+
+    wasInitRef.current = true;
+
+    const swiper = new Swiper(swiperElRef.current, {
+      modules: [Virtual, Controller],
+      speed: 400,
+      spaceBetween: 16,
+      slidesPerView: 1,
+      // observer: true,
+      simulateTouch: false,
+      touchStartPreventDefault: false,
+      direction: 'vertical',
+      virtual: {
+        enabled: true,
+        addSlidesAfter: 2,
+        addSlidesBefore: 2,
+        // slides: [],
+        // slides: ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5', 'Slide 6', 'Slide 7', 'Slide 8']
+      },
+      on: {
+        afterInit: setSwiperState,
+        slideChange: onSliderChange,
+      },
+    });
+  }, [onSliderChange, setSwiperState]);
 
   return (
     <div className={styles.box}>
@@ -78,7 +119,43 @@ export const ProfileSwiper = observer(({ swiperState, setSwiperState }) => {
         })}
         ref={wrapperRef}
       >
-        <Swiper
+        <div className='swiper' ref={swiperElRef} style={{ height: '100%' }}>
+          <div className='swiper-wrapper'>
+            {/* {swiperStore.isFetchingList && swiperStore.profileList.length === 0 && (
+              <div className={cn(styles.slide, 'swiper-slide')}>
+                <Space className={styles.space} direction='vertical'>
+                  <Spin tip='Loading' size='large'>
+                    <div />
+                  </Spin>
+                </Space>
+              </div>
+            )} */}
+            {swiperStore.profileList.map((profile, profileIndex) => {
+              const dataProps = { [DATA_ATTR_PROFILE_ID]: profile.id };
+
+              return (
+                <div
+                  className='swiper-slide'
+                  key={profile.id}
+                  // virtualIndex={profileIndex}
+                  {...dataProps}
+                >
+                  <ProfilePreview profileData={profile} />
+                  {swiperStore.isFetchingList &&
+                    profileIndex === swiperStore.profileList.length - 1 && (
+                      <div className={styles['last-loader-wrapper']}>
+                        <Spin size='default' />
+                      </div>
+                    )}
+                </div>
+              );
+            })}
+            {/* <div className='swiper-slide'>Slide 11</div>
+            <div className='swiper-slide'>Slide 22</div>
+            <div className='swiper-slide'>Slide 33</div> */}
+          </div>
+        </div>
+        {/* <Swiper
           direction='vertical'
           style={{ height: '100%' }}
           observer
@@ -144,7 +221,7 @@ export const ProfileSwiper = observer(({ swiperState, setSwiperState }) => {
               </SwiperSlide>
             );
           })}
-        </Swiper>
+        </Swiper> */}
       </div>
       <BottomBlock />
     </div>
